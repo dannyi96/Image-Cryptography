dcfbc1cc49998368e72551af0330432a75c030ef
Daniel Isaac <danielbcbs2@gmail.com>
2023-11-18 00:35:58 +0530

    webapp fix - download encrypted pic

--- webapp/pyscript/image_crypto.py
+++ webapp/pyscript/image_crypto.py
@@ -8,20 +8,13 @@ from pyscript import when, display, document
 def click_handler(event):
     image_prefix, image_data = document.getElementById('input-image').src.split('base64,')
     image_bytes = base64.b64decode(image_data)
-    print(image_bytes)
     input_image = Image.open(BytesIO(image_bytes))
-    print(input_image)
     encryptor = RubikCubeCrypto(input_image)
-    print('HERE')
-    encrypted_image = encryptor.encrypt(alpha=8, iter_max=1, key_filename='key.txt')
-    print(encryptor.encoded_key)
-    print('HERE 2')
-    print('HERE 3')
+    encrypted_image = encryptor.encrypt(alpha=8, iter_max=1)
     output_buffer = BytesIO()
     encrypted_image.save(output_buffer, format=input_image.format)
     image_bytes = output_buffer.getvalue()
     encoded_image = base64.b64encode(image_bytes).decode('utf-8')
-    print(encoded_image)
     document.getElementById('output-image').src = image_prefix + 'base64,' + encoded_image
     document.getElementById('encoded-key').innerText = encryptor.encoded_key
     
