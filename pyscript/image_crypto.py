1e547611b3ec6c24658ca78f8ad8adb5dd5d1d6c
dannyi96 <danielbcbs2@gmail.com>
2023-11-17 19:49:14 +0530

    github page init

---
+++ pyscript/image_crypto.py
@@ -0,0 +1,25 @@
+from rubikencryptor.rubikencryptor import RubikCubeCrypto
+from PIL import Image
+from io import BytesIO
+import base64
+from pyscript import when, display, document
+
+@when("click", "#transform-image")
+def click_handler(event):
+    image_prefix, image_data = document.getElementById('input-image').src.split('base64,')
+    image_bytes = base64.b64decode(image_data)
+    print(image_bytes)
+    input_image = Image.open(BytesIO(image_bytes))
+    print(input_image)
+    encryptor = RubikCubeCrypto(input_image)
+    print('HERE')
+    encrypted_image = encryptor.encrypt(alpha=8, iter_max=1, key_filename='key.txt')
+    print(encryptor.encoded_key)
+    print('HERE 2')
+    print('HERE 3')
+    output_buffer = BytesIO()
+    encrypted_image.save(output_buffer, format=input_image.format)
+    image_bytes = output_buffer.getvalue()
+    encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+    print(encoded_image)
+    document.getElementById('output-image').src = image_prefix + 'base64,' + encoded_image
