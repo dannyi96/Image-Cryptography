6830e4f36dcc81c0fa47efc1961d52f3df78dc4f
dannyi96 <danielbcbs2@gmail.com>
2023-11-18 03:54:12 +0530

    package upgrade

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="rubikencryptor",
-    version="1.0.7",
+    version="1.0.8",
     author="Daniel Isaac",
     author_email="danielbcbs2@gmail.com",
     description="Image Cryptography Based on Rubix's Cube Principle",
