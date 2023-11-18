bd62d8eea31149637b26150a85798f5432ccdd67
dannyi96 <danielbcbs2@gmail.com>
2023-11-18 21:03:45 +0530

    UI phase 1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-function previewImage(event) {
+function previewImage(event, imageContainerId, imageId) {
     const input = event.target;
-    const container = document.getElementById('image-container');
-    const image = document.getElementById('input-image');
+    const container = document.getElementById(imageContainerId);
+    const image = document.getElementById(imageId);
 
     const file = input.files[0];
 
@@ -20,6 +20,44 @@ function previewImage(event) {
     }
 }
 
-function encryptImage(event) {
+function downloadImage(imgId, fileName) {
+    var new_anchor = document.createElement("a")
+    document.body.appendChild(new_anchor);
+    new_anchor.download = fileName;
+    new_anchor.href = document.getElementById(imgId).src;
+    // click the anchor to trigger download
+    new_anchor.click();
+    new_anchor.remove();
+}
 
+function downloadKey(divId, fileName) {
+    var new_anchor = document.createElement("a")
+    document.body.appendChild(new_anchor);
+    new_anchor.download = fileName;
+    new_anchor.href = "data:text/html," + document.getElementById(divId).innerHTML;
+    // click the anchor to trigger download
+    new_anchor.click();
+    new_anchor.remove();
+}
+
+function toggleState(stateIdToEnable, stateIdToDisable) {
+    document.getElementById(stateIdToEnable).style.display = "block";
+    document.getElementById(stateIdToDisable).style.display = "none";
+}
+
+function previewText(event, divContainerId) {
+    const input = event.target;
+    const container = document.getElementById(divContainerId);
+
+    const file = input.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(e) {
+            container.textContent = e.target.result;
+        };
+
+        reader.readAsText(file);
+    }
 }
--- pyscript/image_crypto.py
+++ pyscript/image_crypto.py
@@ -4,22 +4,33 @@ from io import BytesIO
 import base64
 from pyscript import when, display, document
 
-@when("click", "#transform-image")
+
+@when("click", "#encrypt-image")
 def click_handler(event):
     image_prefix, image_data = document.getElementById('input-image').src.split('base64,')
     image_bytes = base64.b64decode(image_data)
-    print(image_bytes)
     input_image = Image.open(BytesIO(image_bytes))
-    print(input_image)
     encryptor = RubikCubeCrypto(input_image)
-    print('HERE')
-    encrypted_image = encryptor.encrypt(alpha=8, iter_max=1, key_filename='key.txt')
-    print(encryptor.encoded_key)
-    print('HERE 2')
-    print('HERE 3')
+    encrypted_image = encryptor.encrypt(alpha=8, iter_max=1)
     output_buffer = BytesIO()
     encrypted_image.save(output_buffer, format=input_image.format)
     image_bytes = output_buffer.getvalue()
     encoded_image = base64.b64encode(image_bytes).decode('utf-8')
-    print(encoded_image)
     document.getElementById('output-image').src = image_prefix + 'base64,' + encoded_image
+    document.getElementById('encrypt-encoded-key').innerText = encryptor.encoded_key
+
+
+@when("click", "#decrypt-image")
+def click_handler(event):
+    image_prefix, image_data = document.getElementById('decrypt-input-image').src.split('base64,')
+    image_bytes = base64.b64decode(image_data)
+    input_image = Image.open(BytesIO(image_bytes))
+    encryptor = RubikCubeCrypto(input_image)
+    keyElem = eval(document.getElementById('decrypt-encoded-key-contents').textContent)
+    encrypted_image = encryptor.decrypt_with_key(keyElem)
+    output_buffer = BytesIO()
+    encrypted_image.save(output_buffer, format=input_image.format)
+    image_bytes = output_buffer.getvalue()
+    encoded_image = base64.b64encode(image_bytes).decode('utf-8')
+    document.getElementById('decrypt-output-image').src = image_prefix + 'base64,' + encoded_image
+    
